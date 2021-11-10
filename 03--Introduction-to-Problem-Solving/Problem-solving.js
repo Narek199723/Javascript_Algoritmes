cbf341c5407b5b20fc112cd1b3a4e51c5835a7c4
Narek199723 <narek.boshyan@gmail.com>
2021-11-10 10:47:24 +0400

    adding files

--- 03--Introduction-to-Problem-Solving/Problem-solving.js
+++ 03--Introduction-to-Problem-Solving/Problem-solving.js
@@ -12,7 +12,6 @@
 //*   Almost everything that you do in programming involves some kind of algorithm !
 // *  It's the foundation for being a successful problem solving and developer
 
-
 // ^  How to improve Problem solving skills ?
 // * 1. Devise a plan for solving problems
 // * 2. Master common problem solving patterns
@@ -23,3 +22,27 @@
 // *  Break it down
 // *  Solve/Simplify
 // *  Look Back and refactor
+
+// ^  Understand the problem
+// * 1. Can i restate the problem in my own words?
+// * 2. What are the inputs that go into the problem
+// * 3. What are the outputs that should come from the solution to the problem
+// * 4. Can the outputs be determined from the inputs? In other words, do i have enough information to solve the problem?(you may not be able to answer this question until you set about solving the problem.That's okay;It's still worth considering the question at this early stage)
+// * 5.How should i label the important pieces of data that are a part of the problem
+
+// ^ Let's write a example and test what we wrote above
+// ^ Write a function which takes two numbers and returns their sum
+
+// % 1. Can i restate the problem in my own words?
+// * Implement addition
+// % 2. What are the inputs that go into the problem
+// *  --Integers?
+// *  --floats?
+// *  --What about string for large numbers?
+// % 3. What are the outputs that should come from the solution to the problem
+// *  --integers,float,string
+// % 4. Can the outputs be determined from the inputs? In other words, do i have enough information to solve the problem?(you may not be able to answer this question until you set about solving the problem.That's okay;It's still worth considering the question at this early stage)
+// % 5.How should i label the important pieces of data that are a part of the problem
+
+
+// ! Concrete Examples 
